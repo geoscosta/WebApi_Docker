94cbf6d7614e3cf49e2a7c6edad26627eb06c623
Geovane Silva <geoscosta@gmail.com>
2022-01-24 09:50:33 -0300

    Subindo API

---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Model;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private List<UserModel> users = null;
+
+        public UserController()
+        {
+            users = new List<UserModel>()
+            {
+                new UserModel() { Id = 1, Name = "João"},
+                new UserModel() { Id = 2, Name = "Maria"}
+            };
+        }
+
+        public IActionResult GetUsers()
+        {
+            return Ok(users);
+        }
+
+        [Route("{id:int}")]
+        public IActionResult GetUserById(int id)
+        {
+            var user = users.FirstOrDefault(x => x.Id == id);
+
+            if (user == null)
+                return NotFound();
+
+            return Ok(user);
+        }
+    }
+}
---
+++ WebAPI/Model/UserModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace WebAPI.Model
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
